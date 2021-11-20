6ed9eeebc31d24bdea6c404ab0921a9a08da73de
karam084 <karam.ezz84@gmail.com>
2021-11-20 17:46:46 +0200

    Add javascript

---
+++ js/mobile-menu.js
@@ -0,0 +1,54 @@
+const closeMenu = document.querySelectorAll('.mobile-menu-toggle');
+
+closeMenu.forEach((element) => {
+  element.addEventListener('click', () => {
+    const overlay = document.querySelector('.menu-overlay');
+    overlay.classList.toggle('hide-menu');
+
+    const hamburgerMenu = document.querySelector('.mobile-menu');
+    hamburgerMenu.classList.toggle('hide-hamburger');
+  });
+});
+
+const speakers = [
+  {
+    image: '/src-image/drzekri.jpg',
+    name: 'Abdel Rahman',
+    description: ' Vice President for Reasearch and Postgraduates Affairs',
+  },
+  {
+    image: '/src-image/profa.jpg',
+    name: 'Mohamed Samy ',
+    description:
+      ' Cairo University Vice President for Community Service and Environment Development Affairs',
+  },
+  /* {
+    image: '/src-image/salma.jpg',
+    name: 'Salima Ikram',
+    description:
+      'Distinguished University Professor of Egyptology at The American University in Cairo',
+  },
+  {
+    image: '/src-image/nagw.jpg',
+    name: 'Nagwa Sherif',
+    description:
+      'professor of Architectural Engineering at The American University in Cairo',
+  },*/
+];
+
+speakers.forEach((speaker) => {
+  const div = document.querySelector('.feature-speak');
+  const dynamicContent = `
+     <card class="speaker ">
+                <img src=${speaker.image} alt="Speaker" class="speaker-img">
+                <div class="speaker-info">
+                  <h3 class="speaker-name">${speaker.name}</h3>
+                  <p class="speaker-desc">${speaker.description}
+                  </p>
+                  <p>Holds a masters in Divinity from Afric Inland University.</p>
+                </div>
+              </card>
+            </div>
+`;
+  div.innerHTML += dynamicContent;
+});
